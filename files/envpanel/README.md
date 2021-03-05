1722fcebcf75dffd7f7765ad41091ce7e156a49e
Jens Scheidtmann <jens.scheidtmann@gmail.com>
2021-03-05 18:37:20 +0100

    Skeleton for envpanel created.

---
+++ files/envpanel/README.md
@@ -0,0 +1,69 @@
+# Environment Panel
+
+Environment Panel is a simple web application to present information from a BME280 server. It depends on a BME280 sensor connected to I2C, and reads environment data from that sensor.
+
+
+# Installation
+
+Env Panel is based on [Python Flask](http://flask.pocoo.org/) micro-framework. It has a built-in webserver and by default listens on port 8627. Install the pre-requisites:
+
+```
+$sudo pip3 install -r requirements.txt 
+```
+
+Copy the **envpanel** folder to  **DIRECTORY** (eg. /var/www/)
+
+# Usage
+
+The Env Panel can be run as a standalone server. It can be started manually by invoking python:
+
+```
+$ python3 DIRECTORY/envpanel/envpanel.py
+```
+
+Then using your favorite web browser, go to http://your_ip_address:8627
+
+# Auto Start
+
+To enable the Env Panel to automatically start after a system reboot, a systemd service file is provided for your convenience:
+
+```
+[Unit]
+Description=Env Panel
+After=multi-user.target
+
+[Service]
+Type=idle
+User=nobody
+ExecStart=/usr/bin/python3 DIRECTORY/envpanel/envpanel.py
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+```
+
+The above service files assumes you copied the envpanel directory to **DIRECTORY** - replace **DIRECTORY** with real path to gpspanel on your system.
+
+Copy the envpanel.service file to **/etc/systemd/system**:
+
+```
+sudo cp envpanel.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/envpanel.service
+```
+
+Now configure systemd to load the service file during boot:
+
+```
+sudo systemctl daemon-reload
+sudo systemctl enable envpanel.service
+sudo systenctl start envpanel.service
+```
+
+After startup, check the status of the Env Panel service:
+
+```
+sudo systemctl status envpanel.service
+```
+
+If all appears OK, you can start using Env Panel using any browser.
---
+++ files/envpanel/envpanel.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+#  Copyright(c) 2017 Radek Kaczorek  <rkaczorek AT gmail DOT com>
+#  Copyright(c) 2021 Jens Scheidtmann <Jens.Scheidtmann AT gmail DOT com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this library; see the file COPYING.LIB.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+# from gevent import monkey; monkey.patch_all()
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from PIL import Image, ImageDraw
+import time, base64, math, io, sys
+
+__author__ = 'Jens Scheidtmann'
+__copyright__ = 'Copyright 2021  Jens Scheidtmann'
+__license__ = 'GPL-3'
+__version__ = '0.1.0'
+
+app = Flask(__name__, static_folder='assets')
+socketio = SocketIO(app)
+thread = None
+
+
+def background_thread():
+	""" Read environmental sensor from I2C bus, a BME280 is expected """
+	for new_data in gpsd_socket:
+		if new_data:
+			# TODO: Read output from BME280.
+			pass
+		else:
+			time.sleep(5)
+
+
+	# from gpspanel, see there
+	# encode and return
+	imgdata = io.BytesIO()
+	img.save(imgdata, format="PNG")
+	imgdata_encoded = base64.b64encode(imgdata.getvalue()).decode()
+	return imgdata_encoded
+
+def shut_down():
+    print('Keyboard interrupt received\nTerminated by user\nGood Bye.\n')
+    sys.exit(1)
+
+
+@app.route('/')
+def main():
+	return render_template('main.html')
+
+@socketio.on('connect')
+def handle_connect():
+	global thread
+	if thread is None:
+		thread = socketio.start_background_task(target=background_thread)
+
+if __name__ == '__main__':
+	# TODO: Create rrdtool files, not overwriting them
+
+	try:
+		socketio.run(app, host='0.0.0.0', port = 8627, debug=False)
+	except KeyboardInterrupt:
+		shut_down()
