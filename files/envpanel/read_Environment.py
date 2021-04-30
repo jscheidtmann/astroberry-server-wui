6967133d2e69f1fce5cb1f7b0df9a6bf4b0b9d47
Jens Scheidtmann <jens.scheidtmann@gmail.com>
2021-05-01 00:36:24 +0200

    Read environment, computer and power state in one go and store in RRD.

--- files/envpanel/read_Environment.py
+++ files/envpanel/read_Environment.py
@@ -4,9 +4,61 @@
 # Run this file using:
 # $ pyhton3 read_Environment.py
 
+# Check, if a RRDTool database exists, if not, create it
+
+import os.path
+import rrdtool
+
+env = '/var/local/astroberry/environment.rrd'
+cpu = '/var/local/astroberry/computer.rrd'
+power = '/var/local/astroberry/power.rrd'
+
+# Environmental readings (temperature, pressure and humidity) are collected every ten seconds.
+
+if not os.path.exists(env):
+    print("Creating environment.rrd")
+    rrdtool.create(env, '--start', 'now', '--step', '10s', 
+        'DS:temp:GAUGE:30s:U:U',
+        'DS:pressure:GAUGE:30s:U:U',
+        'DS:humidity:GAUGE:30s:U:U',
+        'RRA:AVERAGE:0.5:1:1d',
+        'RRA:AVERAGE:0.5:5m:14d',
+        'RRA:AVERAGE:0.5:15m:60d',
+        'RRA:AVERAGE:0.5:30m:180d')
+
+if not os.path.exists(cpu):
+    print("Creating computer.rrd")
+    rrdtool.create(cpu, '--start', 'now', '--step', '1s',
+        'DS:cpupercent:GAUGE:5s:U:U',
+        'DS:cputemp:GAUGE:5s:U:U',
+        'DS:load1:GAUGE:5s:U:U',
+        'DS:load5:GAUGE:5s:U:U',
+        'DS:load15:GAUGE:5s:U:U',
+        'RRA:AVERAGE:0.5:1:1d',
+        'RRA:AVERAGE:0.5:1m:7d',
+        'RRA:AVERAGE:0.5:5m:14d',
+        'RRA:AVERAGE:0.5:15m:60d',
+        'RRA:AVERAGE:0.5:30m:180d')
+
+if not os.path.exists(power):
+    print("Creating power.rrd")
+    rrdtool.create(power, '--start', 'now', '--step', '1s',
+        'DS:voltage:GAUGE:5s:U:U',
+        'DS:current:GAUGE:5s:U:U',
+        'DS:power:GAUGE:5s:U:U',
+        'RRA:AVERAGE:0.5:1:1d',
+        'RRA:AVERAGE:0.5:5m:14d',
+        'RRA:AVERAGE:0.5:15m:60d',
+        'RRA:AVERAGE:0.5:30m:180d')
+
 import smbus2
 import bme280
+import time
 
+###################
+#   Environment   #
+###################
+# 
 # Connect to the bme280 sensor connected on the I2C bus (SMBus)
 # The standard address is given here. 0x76 is the standard address of a bme280. If you used a different one,
 # check which address is used by running 
@@ -18,16 +70,51 @@ bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
 
-# the sample method will take a single reading and return a
-# compensated_reading object
-data = bme280.sample(bus, address, calibration_params)
+#####################
+#   Power Reading   #
+#####################
 
-# the compensated_reading class has the following attributes
-print(data.id)
-print(data.timestamp)
-print(data.temperature)
-print(data.pressure)
-print(data.humidity)
+from ina260.controller import Controller
 
-# there is a handy string representation too
-print(data)
+# Read Voltage and Current readings from hte ina260 board, that is connected via I2C to your RaspPi
+# 0x40 is the standard address used by the device, if you're using a different adress, change it here.
+# Check which address your device is using by running:
+# $ i2cdetect -y 1
+
+c = Controller(address=0x40)
+
+#######################
+#   State of the PI   #
+#######################
+
+import psutil
+import os
+
+############################
+#   Collect informations   # 
+############################
+# 
+# ad infinitum
+#  
+i = 0
+while True:
+    # Every 10 power samples, sample once the environment.
+    if i % 10 == 0: 
+        # the sample method will take a single reading and return a
+        # compensated_reading object
+        data = bme280.sample(bus, address, calibration_params)
+
+        # print(f"{data.timestamp}: t={data.temperature}, p={data.pressure}, h={data.humidity}")
+        rrdtool.update(env, f'N:{data.temperature}:{data.pressure}:{data.humidity}')
+
+    # Power
+    rrdtool.update(power, f'N:{c.voltage()}:{c.current()}:{c.power()}')
+
+    # Pi State
+    info= psutil.sensors_temperatures()
+    avgs = os.getloadavg()
+    rrdtool.update(cpu, f"N:{psutil.cpu_percent()}:{info['cpu_thermal'][0].current}:{avgs[0]}:{avgs[1]}:{avgs[2]}")
+
+    i = i+1
+    time.sleep(1)
+
